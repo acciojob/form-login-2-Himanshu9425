08162c05020fd0df777be325ef263afc2009c7d6
Himanshu9425 <147365994+Himanshu9425@users.noreply.github.com>
2025-02-26 11:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function showalert() {
+let first=document.querySelector("#FirstName");
+let last=document.querySelector("#LastName");
+let phone=document.querySelector("#PhoneNumber");
+let email=document.querySelector("#EmailId");
+
+let message = "firstName:"+first.value+"LastName:"+last.value+"PhoneNumber:"+phone.value+"EmailID:"+email.value
+
+	alert(message);
+}
